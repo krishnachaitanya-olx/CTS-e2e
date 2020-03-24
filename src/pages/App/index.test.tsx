74826e66a0e6a97276c3110fa1a13ddf13710270
sujay-aj <sujay.mondal@olxpeople.com>
2020-03-24 19:36:36 +0530

    E2E-1634:added SKIP_PREFLIGHT_CHECK=true for test and run

--- src/pages/App/index.test.tsx
+++ src/pages/App/index.test.tsx
@@ -1,9 +1,26 @@
 import App from './index';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
+
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  console.log('\n\n',typeof(linkElement));
+  expect(linkElement).toMatchSnapshot();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).not.toEqual(HTMLElement);
 });
