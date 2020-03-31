6d05922a0c118bc68fe4c5523219ec7b117f68df
Ameer Jhan <ameer.jhan@olxpeople.com>
2020-03-31 14:18:49 +0530

    test: add tests for app component

---
+++ src/grids/Listings/Listings.test.tsx
@@ -0,0 +1,8 @@
+import ListingGrid from './Listings.component';
+import { render } from '@testing-library/react';
+
+describe('ListingGrid', () => {
+    const mocks = [{
+        
+    }]
+});
--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import App from './App.component';
+import { render, wait } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import history from 'utils/misc/history';
 
 describe('App', () => {
-    it('should work', () => {
-        expect(1).toBe(1);
+    it('should set the page title', async () => {
+        render(
+            <Router history={history}>
+                <App />
+            </Router>
+        );
+
+        await wait(() => expect(document.title).toBe('CTS'));
+    });
+
+    it('should set the required meta tags', async () => {
+        render(
+            <Router history={history}>
+                <App />
+            </Router>
+        );
+
+        await wait(() => {
+            const metaTag = document.querySelector('meta[name="description"]');
+            const content = metaTag? metaTag.getAttribute('content'): null;
+
+            expect(content).toBe('CMS for Aasaanjobs');
+        });
     });
 });
