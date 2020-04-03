a09122b441823562c3694fabdcec904d29ba9289
Ameer Jhan <ameer.jhan@olxpeople.com>
2020-04-03 13:38:31 +0530

    feat: add antd to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { addParameters } from '@storybook/react';
+// import '../src/antd.customize.less';
+import 'antd/dist/antd.less';
 
 addParameters({
-  viewport: {
-    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
-    defaultViewport: 'someDefault',
-  },
+    viewport: {
+        viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+        defaultViewport: 'someDefault',
+    },
 });
