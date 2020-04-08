c8392392185f58f40715b798e4fd9e84536c578e
Ameer Jhan <ameer.jhan@olxpeople.com>
2020-04-08 11:41:31 +0530

    add custom hook for vh

--- src/components/ListingTable/ListingTable.component.tsx
+++ src/components/ListingTable/ListingTable.component.tsx
@@ -2,25 +2,29 @@
 import React, { FC } from 'react';
 import { VirtualTableProps } from 'components/VirtualTable/interface';
 import VirtualTable from 'components/VirtualTable/VirtualTable.component';
+import useVh from 'hooks/useVh';
 
 const ListingTable: FC<VirtualTableProps<any>> = ({
     columns,
     dataSource,
     ...rest
-}: VirtualTableProps<any>) => (
+}: VirtualTableProps<any>) => {
+  const vh = useVh(70);
+  return (
     <VirtualTable
       columns={columns}
       dataSource={dataSource}
       pagination={false}
       bordered
       tableLayout='fixed'
-      scroll={{ y: 700, x: '100vw' }}
+      scroll={{ y: vh, x: '100vw' }}
       style={{
             margin: 25,
             border: 'solid 1px #d4e7fc',
         }}
       {...rest}
     />
-);
+  );
+};
 
 export default ListingTable;
